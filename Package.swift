530cc934d5f982d6e300a213632498aaebda7b0d
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-10-08 16:40:44 +0000

    Update 75.1.2-DEV.48

--- Package.swift
+++ Package.swift
@@ -14,48 +14,48 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ffmpegkit",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.2/ffmpegkit.xcframework.zip",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.3/ffmpegkit.xcframework.zip",
             checksum: "c46909a8c1d21eac04c31376fdc771f9570e65acb63ececa1fbc510d544d0711"
         ),
         .binaryTarget(
             name: "libavcodec",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.2/libavcodec.xcframework.zip",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.3/libavcodec.xcframework.zip",
             checksum: "17e7f49ef66f723f84fa4655f70f15907695cbcf1a7c7cb10d0b040205f32874"
         ),
         .binaryTarget(
             name: "libavdevice",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.2/libavdevice.xcframework.zip",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.3/libavdevice.xcframework.zip",
             checksum: "3d54f55cbe3d6b9be5d44ba07028e7cf8910e8b5b0d3b50c5fc3672770e0c9d7"
         ),
         .binaryTarget(
             name: "libavfilter",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.2/libavfilter.xcframework.zip",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.3/libavfilter.xcframework.zip",
             checksum: "a9ebd1652e57fb5719037bb91197cf3c357ecffa2b28d9f0ba4f37660c30b16a"
         ),
         .binaryTarget(
             name: "libavformat",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.2/libavformat.xcframework.zip",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.3/libavformat.xcframework.zip",
             checksum: "5bd2bb4c22275c6afda60ac30dc15e7ce5c6d708c09108cd549a0433c901a7bb"
         ),
         .binaryTarget(
             name: "libavutil",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.2/libavutil.xcframework.zip",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.3/libavutil.xcframework.zip",
             checksum: "2bef86cc9797a320423e04af351ab45eff0546a9eba3208fae3aa81bb8a99114"
         ),
         .binaryTarget(
             name: "libswresample",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.2/libswresample.xcframework.zip",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.3/libswresample.xcframework.zip",
             checksum: "d81b332982467ec10d1c6fe9c66cb54764ef53fdb0c3072de8a6a7b8a9770cb4"
         ),
         .binaryTarget(
             name: "libswscale",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.2/libswscale.xcframework.zip",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-video-utils/releases/download/0.0.3/libswscale.xcframework.zip",
             checksum: "8a7d21ecf1620acf6e806beb6c1cd2f42cf8a1b98cd64ef071287d68f0bdc198"
         ),
         .binaryTarget(
             name: "TruvideoSdkVideo",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-video/releases/download/73.1.3-RC.36/TruvideoSdkVideo.xcframework.zip",
-            checksum: "64024a3adcd2c68106126856e61413e0a8e0d86ee87d27d7597a83a15261de0d"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-video/releases/download/75.1.2-DEV.48/TruvideoSdkVideo.xcframework.zip",
+            checksum: "76f9cc55d2efcef3fcaea2c60536cc4684189db01b12f039347107e554b93a60"
         ),
         .target(
             name: "TruvideoSdkVideoTargets",
